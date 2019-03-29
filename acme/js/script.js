f40b5e3fb300c9ed05d1e940359e1a3ce59957cb
Durfinator <harmonydurfee@gmail.com>
2019-03-29 11:12:44 -0600

    Updating script.js

--- acme/js/script.js
+++ acme/js/script.js
@@ -1,10 +1,40 @@
 //General variables and calling functions
-fetchData('Anvils');
-
+const s = localStorage;
 
 //Fetching the JSON data
 let json = 'https://durfinator.github.io/acme/js/acme.json';
-function fetchData(page) {
+// fetchData('Anvils', json);
+// fetchData('Explosives', json);
+// function fetchData(page, json) {
+//   fetch(json)
+//     .then(function (response) {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//       throw new ERROR('Network response was not OK.');
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       let d = data[page];
+//       s.setItem('name', d.Name);
+//       s.setItem('path', d.Path);
+//       s.setItem('description', d.Description);
+//       s.setItem('manufacturer', d.Manufacturer);
+//       s.setItem('price', d.Price);
+//       s.setItem('reviews', d.Reviews);
+//       console.log(d);
+//     })
+//     .catch(function (error) {
+//       console.log('There was a fetch problem: ', error.message);
+//       statusContainer.innerHTML = 'Sorry, the data could not be processed.';
+//     })
+// }
+
+//Function to build the navigation
+buildNav(json);
+function buildNav(json) {
+  let nav = document.getElementById('page-nav');
+  // Build the remaining list items using a for loop
   fetch(json)
     .then(function (response) {
       if (response.ok) {
@@ -14,28 +44,14 @@ function fetchData(page) {
     })
     .then(function (data) {
       console.log(data);
-      let g = data[page];
-      let name = g.Name;
-      let path = g.Path;
-      let description = g.Description;
-      let manufacturer = g.Manufacturer;
-      let price = g.Price;
-      let reviews = g.Reviews;
-      console.log(g);
+      let d = data['Titles'];
+      console.log(d);
+      nav.innerHTML += '<li><a href="#" title="Home">Home</a></li>';
+      for (let i = 1; i < 5; i++) {
+        nav.innerHTML += '<li><a href="#" title="' + d.i + '">' + d.i + '</a></li>';
+      }
     })
     .catch(function (error) {
       console.log('There was a fetch problem: ', error.message);
-      statusContainer.innerHTML = 'Sorry, the data could not be processed.';
     })
-}
-
-//Function to build the navigation
-function buildNav() {
-    let navList = '<li>' + format_time(nextHour) + '</li>';
-    // Build the remaining list items using a for loop
-    for (let i = 1, x = hourlyTemps.length; i < x; i++) {
-        navList += '<li> | ' + format_time(nextHour + i) + '</li>';
-    }
-    console.log('HourlyList is: ' + navList);
-    return navList;
 }
